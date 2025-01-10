25f5ffc47f9a822817a562d3252299e9c2742a8a
Martin Lindström <martin.lindstrom@litsec.se>
2025-01-10 09:35:16 +0100

    2.3.2

--- docs/apidoc/package-search-index.js
+++ docs/apidoc/package-search-index.js
@@ -1,16 +1,1 @@
-/*
- * Copyright 2023-2025 Sweden Connect
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"se.swedenconnect.spring.saml.idp"},{"l":"se.swedenconnect.spring.saml.idp.attributes"},{"l":"se.swedenconnect.spring.saml.idp.attributes.eidas"},{"l":"se.swedenconnect.spring.saml.idp.attributes.nameid"},{"l":"se.swedenconnect.spring.saml.idp.attributes.release"},{"l":"se.swedenconnect.spring.saml.idp.audit"},{"l":"se.swedenconnect.spring.saml.idp.audit.data"},{"l":"se.swedenconnect.spring.saml.idp.audit.repository"},{"l":"se.swedenconnect.spring.saml.idp.authentication"},{"l":"se.swedenconnect.spring.saml.idp.authentication.provider"},{"l":"se.swedenconnect.spring.saml.idp.authentication.provider.external"},{"l":"se.swedenconnect.spring.saml.idp.authnrequest"},{"l":"se.swedenconnect.spring.saml.idp.authnrequest.validation"},{"l":"se.swedenconnect.spring.saml.idp.authnrequest.validation.replay"},{"l":"se.swedenconnect.spring.saml.idp.config"},{"l":"se.swedenconnect.spring.saml.idp.config.configurers"},{"l":"se.swedenconnect.spring.saml.idp.context"},{"l":"se.swedenconnect.spring.saml.idp.error"},{"l":"se.swedenconnect.spring.saml.idp.events"},{"l":"se.swedenconnect.spring.saml.idp.extensions"},{"l":"se.swedenconnect.spring.saml.idp.metadata"},{"l":"se.swedenconnect.spring.saml.idp.response"},{"l":"se.swedenconnect.spring.saml.idp.settings"},{"l":"se.swedenconnect.spring.saml.idp.utils"},{"l":"se.swedenconnect.spring.saml.idp.web.filters"}];updateSearchResults();
