6088dc5967a0b11358a080b38f455787b4eafd38
Martin Lindström <martin-lindstrom@users.noreply.github.com>
2025-03-26 15:55:28 +0100

    2.3.8 (#121)
    Dependency updates only

--- saml-identity-provider/src/main/java/se/swedenconnect/spring/saml/idp/Saml2IdentityProviderVersion.java
+++ saml-identity-provider/src/main/java/se/swedenconnect/spring/saml/idp/Saml2IdentityProviderVersion.java
@@ -24,7 +24,7 @@ public final class Saml2IdentityProviderVersion {
 
   private static final int MAJOR = 2;
   private static final int MINOR = 3;
-  private static final int PATCH = 7;
+  private static final int PATCH = 8;
 
   /**
    * Global serialization value for SAML Identity Provider classes.
