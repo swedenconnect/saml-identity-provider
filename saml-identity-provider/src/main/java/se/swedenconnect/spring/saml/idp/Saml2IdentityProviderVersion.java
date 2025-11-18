09ae810edc88c070707b37d1f604fb47971e54bd
Martin Lindström <martin.lindstrom@litsec.se>
2025-11-18 15:15:33 +0100

    2.3.10

--- saml-identity-provider/src/main/java/se/swedenconnect/spring/saml/idp/Saml2IdentityProviderVersion.java
+++ saml-identity-provider/src/main/java/se/swedenconnect/spring/saml/idp/Saml2IdentityProviderVersion.java
@@ -24,7 +24,7 @@ public final class Saml2IdentityProviderVersion {
 
   private static final int MAJOR = 2;
   private static final int MINOR = 3;
-  private static final int PATCH = 9;
+  private static final int PATCH = 10;
 
   /**
    * Global serialization value for SAML Identity Provider classes.
